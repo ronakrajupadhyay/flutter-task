e950d707738aadf3290335fbe450b0a9a229fe2e
ronak <ronakupadhyay36@gmail.com>
2025-12-23 11:32:28 +0530

    variable add

---
+++ flutterTask/vriable.dart
@@ -0,0 +1,14 @@
+import 'dart:ffi';
+
+// in dart we have int , float , boolean ,String data type
+
+void main() {
+  int a=5;
+  int b=10;
+  int sum =a+b;
+  print(sum);
+
+  String c ="Ronak";
+
+
+}
