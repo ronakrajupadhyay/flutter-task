2d36ecbccd611e3a76a3a620dcb6da1ff55035aa
ronak <ronakupadhyay36@gmail.com>
2025-12-23 11:40:06 +0530

    variable add

--- flutterTask/vriable.dart
+++ flutterTask/vriable.dart
@@ -1,14 +1,28 @@
 import 'dart:ffi';
+import 'dart:nativewrappers/_internal/vm/lib/typed_data_patch.dart';
 
 // in dart we have int , float , boolean ,String data type
 
 void main() {
   int a=5;
   int b=10;
-  int sum =a+b;
-  print(sum);
-
   String c ="Ronak";
+  print(c);
+
+  int sum =a+b;
+  int sum1 =a-b;
+  int sum2=a*b;
+  double sum3= a/b;
+  int sum4=a%b;
+
+  print(sum);
+  print(sum1);
+  print(sum2);
+  print(sum3);
+  print(sum4);
+
+
+
 
 
 }
